7405582b630bc7c812f8475528ce418f2bd3a3dd
Daniel Lucas <daniel.lucasafj@gmail.com>
2021-11-04 10:28:12 -0300

    adicionado cors

--- api/services/PacienteService.js
+++ api/services/PacienteService.js
@@ -1,8 +1,10 @@
 const database = require("../db");
 const express = require("express");
+const cors = require('cors')
 const { response } = require("express");
 const app = express();
 
+app.use(cors())
 app.use(express.json());
 
 /**
